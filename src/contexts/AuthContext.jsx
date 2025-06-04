682a5106a63aa30f34f528ace657d58d78db3fb9
Bisounours <yan.dai@outlook.fr>
2025-06-04 23:19:39 +0200

    Ajout de dotenv et mise à jour de la configuration Firebase pour utiliser des variables d'environnement. Mise à jour du fichier .gitignore pour inclure .env.

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -19,13 +19,13 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDze1LjysMb5lfYtnOALNzV12Axv-d8M7U",
-  authDomain: "minecraftcliver-b6796.firebaseapp.com",
-  projectId: "minecraftcliver-b6796",
-  storageBucket: "minecraftcliver-b6796.firebasestorage.app",
-  messagingSenderId: "443793790673",
-  appId: "1:443793790673:web:c2a9c0c6d808306b8157f0",
-  measurementId: "G-7786VVC39P",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const debug = false;
